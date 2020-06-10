eef978cafd6ca4067993bbf460705897b68ecc32
SCH-sys <40889149+UISJOKE@users.noreply.github.com>
2020-06-10 23:08:52 +0300

    Update random datetime.py

--- random datetime.py
+++ random datetime.py
@@ -2,15 +2,16 @@ import random
 import datetime
 
 """Cловарь телефонных кодов Беларуси,России,Украины"""
-country_code = {'Belarus': '+375', 'Russia': '+7', 'Ukraine': '+380'}
+countries_codes = {'Belarus': '+375', 'Russia': '+7', 'Ukraine': '+380'}
 
 """Коды операторов связи Беларуси,России,Украины"""
-operator_code = {'Kievstar': '96', 'Vodafone': '95', 'lifecell': '93', 'Beeline': '903', 'Mts_ru': '910',
+operators_codes = {'Kievstar': '96', 'Vodafone': '95', 'lifecell': '93', 'Beeline': '903', 'Mts_ru': '910',
                  'Megafon': '925', 'Mts_by': '33', 'A1': '29', 'life': '25'}
 
 """Спрашиваем у пользователя Страну и оператора связи"""
 country = input('Belarus,Russia,Ukraine? : ')
 operator = input('Mts_by, A1, life, Mts_ru, Beeline, Megafon, Kievstar, Vodafone, lifecell ? : ')
+contries_with_operators = {'Belarus': ['Mts_by', 'A1', 'life'], 'Russia': ['Mts_ru', 'Beeline', 'Megafon'], 'Ukraine': ['Vodafone', 'Kievstar', 'lifecell']}
 
 
 def random_date_birth():
@@ -22,34 +23,22 @@ def random_date_birth():
 
 
 def random_phone_number():
-    """Функция принимает проверяет значения переменных country и operator введенных пользователем и генерирует
-    случайный номер телефона с указаными одами стран(country) и кодами операторов(operator) """
-    if country == 'Belarus':
-        if operator == 'Mts_by' or operator == 'A1' or operator == 'life':
-            number = country_code.get(country) + operator_code.get(operator) + str(random.randint(1000000, 9999999))
-            return number
+    if country in contries_with_operators.keys():
+        if operator in operators_codes.keys():
+            if operator in contries_with_operators.get(country, []):
+                return countries_codes[country] + operators_codes[operator] + str(random.randint(1000000, 9999999))
+            else:
+                return 'Unknow country in this operator'
         else:
-            return 'Неверный оператор'
-    if country == 'Russia':
-        if operator == 'Mts_ru' or operator == 'Beeline' or operator == 'Megafon':
-            number = country_code.get(country) + operator_code.get(operator) + str(random.randint(1000000, 9999999))
-            return number
-        else:
-            return 'Неверный оператор!'
-    if country == 'Ukraine':
-        if operator == 'Vodafone'or operator == 'Kievstar' or operator == 'lifecell':
-            number = country_code.get(country) + operator_code.get(operator) + str(random.randint(1000000, 9999999))
-            return number
-        else:
-            return 'Неверный оператор!'
+            return 'Unknown operator'
     else:
-        return 'Неверная страна!'
+        return 'Unknown country'
 
 def add():
     for contact in dict_a:
         if contact in dict_a:
-            contact.setdefault({'Birth':random_date_birth()})
-            contact.setdefault({'Phone':random_phone_number()})
+            contact.update({'Birth': random_date_birth()})
+            contact.update({'Phone': random_phone_number()})
             return contact
 
 
