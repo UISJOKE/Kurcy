07082030d2099d9b82f8b779d9479b53b8c78e4f
SCH-sys <40889149+UISJOKE@users.noreply.github.com>
2020-06-27 11:56:10 +0300

    Update oop.py

--- oop.py
+++ oop.py
@@ -6,13 +6,13 @@ class Users:
         self.surname = surname
         self.telephone = telephone
 
-class ads:
+class Ads:
     def __init__(self, ad_name, ad_text, ad_pub_date):
         self.ad_name = ad_name
         self.ad_text = ad_text
         self.ad_pub_date = ad_pub_date
 
-class new_ad(ads):
+class New_ad(Ads):
     def __init__(self, ad_name, ad_text, ad_pub_date, author):
         super().__init__(ad_name, ad_text, ad_pub_date)
         self.author = author
@@ -22,13 +22,13 @@ class new_ad(ads):
 
 
 
-class new_user(Users):
+class New_user(Users):
     def __init__(self, nickname, name, surname, telephone):
         super().__init__(nickname, name, surname, telephone)
     def __str__(self):
         return f'User has been add:\nnickname: {self.nickname}\nname: {self.name}\nsurname: {self.surname}\ntelephone: {self.telephone}\n'
 
-user = new_user(input('Input your nickname: '), input('Input your firstname: '), input('Input you surname: '), input('Input your telephone number: '))
+user = New_user(input('Input your nickname: '), input('Input your firstname: '), input('Input you surname: '), input('Input your telephone number: '))
 print(user)
-ad = new_ad(input('Input name ad: '), input('Input ad text: '), datetime.now(tz=None), user.nickname)
+ad = New_ad(input('Input name ad: '), input('Input ad text: '), datetime.now(tz=None), user.nickname)
 print(ad)
